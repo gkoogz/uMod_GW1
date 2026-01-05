ffaf9ff389a5738dcd9ff987ff3795cec0997241
gkoogz <40226161+gkoogz@users.noreply.github.com>
2026-01-05 11:49:20 -0500

    Ensure DX9 build creates output folders

---
+++ uMod_GUI/uMod_XLocaleShim.cpp
@@ -0,0 +1,31 @@
+/*
+This file is part of Universal Modding Engine.
+
+
+Universal Modding Engine is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Universal Modding Engine is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Universal Modding Engine.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <wx/xlocale.h>
+
+#if !defined(wxUSE_XLOCALE) || (wxUSE_XLOCALE == 0) || defined(UMOD_WX_XLOCALE_SHIM)
+wxXLocale& wxXLocale::GetCLocale()
+{
+  static wxXLocale locale;
+  return locale;
+}
+
+void wxXLocale::Free()
+{
+}
+#endif
