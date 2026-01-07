fa92c70655d9896e1c1a60a15efefbeba147606b
gkoogz <40226161+gkoogz@users.noreply.github.com>
2026-01-07 09:24:36 -0500

    Rebuild all outputs in build script

--- build.cmd
+++ build.cmd
@@ -3,8 +3,30 @@ setlocal enabledelayedexpansion
 
 set ROOT_DIR=%~dp0
 
+echo Cleaning uMod DX9 DLL output...
+if exist "%ROOT_DIR%uMod_DX9\bin" (
+  rmdir /S /Q "%ROOT_DIR%uMod_DX9\bin"
+)
+if exist "%ROOT_DIR%uMod_DX9\obj" (
+  rmdir /S /Q "%ROOT_DIR%uMod_DX9\obj"
+)
+
+echo Cleaning uMod GUI output...
+if exist "%ROOT_DIR%uMod_GUI\bin" (
+  rmdir /S /Q "%ROOT_DIR%uMod_GUI\bin"
+)
+if exist "%ROOT_DIR%uMod_GUI\obj" (
+  rmdir /S /Q "%ROOT_DIR%uMod_GUI\obj"
+)
+
 echo Building uMod DX9 DLL...
 pushd "%ROOT_DIR%uMod_DX9"
+nmake -f makefile.vc clean
+if errorlevel 1 (
+  echo DX9 clean failed.
+  popd
+  exit /b 1
+)
 nmake -f makefile.vc
 if errorlevel 1 (
   echo DX9 build failed.
@@ -23,6 +45,12 @@ if exist "%ROOT_DIR%uMod_DX9\bin\uMod_d3d9_DI.dll" (
 
 echo Building uMod GUI...
 pushd "%ROOT_DIR%uMod_GUI"
+nmake -f makefile.vc clean
+if errorlevel 1 (
+  echo GUI clean failed.
+  popd
+  exit /b 1
+)
 nmake -f makefile.vc BUILD=release
 if errorlevel 1 (
   echo GUI build failed.
